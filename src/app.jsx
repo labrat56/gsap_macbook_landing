fe90cce1074c6f261de92c3c4dbaa3b61e74efe3
manish sharma <msmanish407@gmail.com>
2026-01-08 23:02:41 +0530

    Implemented Hero section

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,13 @@
 import NavBar from "./components/navbar";
+import Hero from "./components/Hero.jsx";
 
 const app = () => {
-  return ( <main> <NavBar/>  </main> )
+  return (
+    <main>
+      <NavBar />
+      <Hero />
+    </main>
+  );
 };
 
 export default app;
