be2265f6d85706f12a1a83db683c0c6269650370
manish sharma <msmanish407@gmail.com>
2026-01-09 23:08:29 +0530

    showcase section

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import NavBar from "./components/navbar";
 import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
+import Showcase from "./components/Showcase.jsx";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
@@ -12,6 +13,7 @@ const app = () => {
       <NavBar />
       <Hero />
       <ProductViewer />
+      <Showcase />
     </main>
   );
 };
